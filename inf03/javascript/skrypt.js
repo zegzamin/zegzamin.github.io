8c4f8c4a133ca2702a801d1abd6f822067ed5f44
zegzamin <ZEGzdamin@gmail.com>
2024-05-26 13:23:20 +0200

    Add files via upload

---
+++ inf03/javascript/skrypt.js
@@ -0,0 +1,12 @@
+        function copyToClipboard(button) {
+            const codeContainer = button.nextElementSibling;
+            const textToCopy = codeContainer.innerText;
+            navigator.clipboard.writeText(textToCopy).then(() => {
+                button.textContent = 'Skopiowano!';
+                setTimeout(() => {
+                    button.textContent = 'Kopiuj';
+                }, 2000);
+            }).catch(err => {
+                console.error('Błąd kopiowania', err);
+            });
+        }
